1c562f11609cfd13fdc18d68f6ba0f95e2649dda
nightblade9 <nightbladecodes@gmail.com>
2017-12-09 22:56:29 -0500

    HaxeFlixel 4.3.0 template Proper resolution Helix for play state

---
+++ source/PlayState.hx
@@ -0,0 +1,22 @@
+package;
+
+import flixel.FlxG;
+import flixel.ui.FlxButton;
+import flixel.math.FlxMath;
+import helix.core.HelixSprite;
+import helix.core.HelixState;
+import helix.core.HelixText;
+import helix.data.Config;
+
+class PlayState extends HelixState
+{
+	override public function create():Void
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
