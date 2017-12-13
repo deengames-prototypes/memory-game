75b368a311d9733f3af52c1e4e79acce3d2a0880
nightblade9 <nightbladecodes@gmail.com>
2017-12-12 22:27:54 -0500

    Generate simple sprite patterns

--- source/memorymasjid/states/PlayState.hx
+++ source/memorymasjid/states/PlayState.hx
@@ -13,10 +13,42 @@ class PlayState extends HelixState
 	override public function create():Void
 	{
 		super.create();
+
+		var sprite = new HelixSprite("assets/images/gem-red.png");
+		sprite.x = 300;
+		sprite.y = 100;
+		this.createPattern(sprite, 3);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+	}
+
+	private function createPattern(originalSprite:HelixSprite, numSprites:Int):Array<HelixSprite>
+	{
+		// Simple pattern: staggered three rows of three
+		if (numSprites < 1 || numSprites > 9)
+		{
+			throw('numSprites must be between 2 and 9 inclusive (was: ${numSprites})');
+		}
+
+		var toReturn = new Array<HelixSprite>();
+		toReturn.push(originalSprite);
+		
+		for (i in 1 ... numSprites)
+		{
+			toReturn.push(originalSprite.clone());
+		}
+
+		for (i in 1 ... toReturn.length)
+		{
+			var sprite = toReturn[i];
+			sprite.x = originalSprite.x + ((i % 3) * 1.1 * sprite.width);
+			sprite.y = originalSprite.y + ((i / 3) * 1.1 * sprite.height);
+			add(sprite);
+		}
+
+		return toReturn;
 	}
 }
